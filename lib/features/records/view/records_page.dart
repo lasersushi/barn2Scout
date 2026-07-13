0442f6921b11a86e5a881f82a54a951e38d5bc51
lasersushi <lwalker30@priorypanther.com>
2026-07-13 14:11:58 -0700

    import

--- lib/features/records/view/records_page.dart
+++ lib/features/records/view/records_page.dart
@@ -1,3 +1,4 @@
+import 'package:barn2scout/features/settings/cubit/settings_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
